46bdefa0ef643fbc18bf34040c9e822248a2b30a
Aqib <aqibr777@gmail.com>
2022-08-16 15:52:54 +0500

    01. Generating some test data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Deployment;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Deployment::factory()
+        ->times(10000)
+        ->sequence(function ($sequence) {
+            return [
+                'created_at' => now()->subHour($sequence->index)
+            ];
+        })
+        ->create();
 });
